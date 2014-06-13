8ae1688ecf0be79a7fd8a485d4ee333c69e1c871
Walter <nobody@nowhere.place>
2014-06-12 22:34:29 -0400

    minor changes

--- xstick.py
+++ xstick.py
@@ -48,24 +48,42 @@ class XStick:
         return self.send_command("ID")
     def get_channel(self):
         return self.send_command("CH")
+    def set_channel(self, channel):
+	return self.send_command("CH %s" % channel)
     def get_serial_number(self):
         return self.send_command("SH") + self.send_command("SL")
     def get_is_coordinator(self):
         return self.send_command("CE")
     def get_active_scan(self, duration=2):
-        return self.get_listing("AS %d")
-                            
+        return self.get_listing("AS")
+    def get_mac_mode(self):
+        return self.send_command("MM")                        
+    def get_node_discover(self):
+        return self.get_listing("ND")
+    def get_scan_channels(self):
+	return self.send_command("SC")
+    def get_firmware_version(self):
+        return self.send_command("VR")
+    def get_firmware_version_verbose(self):
+        return self.get_listing("VL")
 def main():
     xstick = XStick()
     xstick.enter_command_mode()
     print 'Success!'
+    print 'Firmware Version: %s' % xstick.get_firmware_version()
+    print '\n'.join(xstick.get_firmware_version_verbose())
     print 'PAN ID: %s' % xstick.get_panid()
     print 'Channel: %s' % xstick.get_channel()
+    print 'Set Channel: %s' % xstick.set_channel('10')
     print 'Serial: %s' % xstick.get_serial_number()
     print 'Is Coordinator: %s' % xstick.get_is_coordinator()
+    print 'MAC Mode: %s' % xstick.get_mac_mode()
+    print 'Scan Channels: %s' % xstick.get_scan_channels()
     print 'Energy Detect'
     print '\n'.join(xstick.get_listing('ED'))
     print 'Active Scan'
     print '\n'.join(xstick.get_active_scan())
+    print 'Node Discover'
+    print '\n'.join(xstick.get_node_discover())
 if __name__ == '__main__':
     main()
